6afe4db0f274a5c84f728bb150b8bd3c30928221
ZZZhaojoe <tingruizhao625@gmail.com>
2026-06-23 20:58:21 -0600

    Add files via upload

---
+++ StatusSetting.java
@@ -0,0 +1,11 @@
+public class StatusSetting {
+    public static boolean start;
+
+    public void setStart() {
+        start = true;
+    }
+
+    public void exit() {
+        start = false;
+    }
+}
